489030e4eb0affc5ad77bf9bd73b5b125daafeaf
Raffaele Sena <raff367@gmail.com>
2023-05-25 14:52:20 -0700

    The issue with rollup was in the test, also added proper test for varius combinations of adjacent (roll/no roll).

--- matrix_test.go
+++ matrix_test.go
@@ -2,12 +2,96 @@ package matrix
 
 import (
 	"fmt"
+	"sort"
 	"testing"
 )
 
 var (
 	m  = New[string](10, 10, false)
 	m2 = New[string](3, 3, false)
+
+	center = []Cell[string]{
+		{X: 0, Y: 0},
+		{X: 0, Y: 1},
+		{X: 0, Y: 2},
+		{X: 1, Y: 0},
+		{X: 1, Y: 2},
+		{X: 2, Y: 0},
+		{X: 2, Y: 1},
+		{X: 2, Y: 2},
+	}
+
+	left = []Cell[string]{
+		{X: 0, Y: 0},
+		{X: 1, Y: 0},
+		{X: 1, Y: 1},
+		{X: 0, Y: 2},
+		{X: 1, Y: 2},
+	}
+
+	left_r = []Cell[string]{
+		{X: 0, Y: 0},
+		{X: 1, Y: 0},
+		{X: 1, Y: 1},
+		{X: 0, Y: 2},
+		{X: 1, Y: 2},
+		{X: 2, Y: 1},
+		{X: 2, Y: 2},
+		{X: 2, Y: 0},
+	}
+
+	right = []Cell[string]{
+		{X: 1, Y: 2},
+		{X: 2, Y: 2},
+		{X: 1, Y: 1},
+		{X: 1, Y: 0},
+		{X: 2, Y: 0},
+	}
+
+	right_r = []Cell[string]{
+		{X: 1, Y: 2},
+		{X: 2, Y: 2},
+		{X: 1, Y: 1},
+		{X: 1, Y: 0},
+		{X: 2, Y: 0},
+		{X: 0, Y: 2},
+		{X: 0, Y: 1},
+		{X: 0, Y: 0},
+	}
+
+	bottomleft = []Cell[string]{
+		{X: 0, Y: 1},
+		{X: 1, Y: 1},
+		{X: 1, Y: 0},
+	}
+
+	topright = []Cell[string]{
+		{X: 1, Y: 2},
+		{X: 1, Y: 1},
+		{X: 2, Y: 1},
+	}
+
+	bottomleft_r = []Cell[string]{
+		{X: 0, Y: 1},
+		{X: 1, Y: 1},
+		{X: 1, Y: 0},
+		{X: 2, Y: 1},
+		{X: 2, Y: 0},
+		{X: 2, Y: 2},
+		{X: 0, Y: 2},
+		{X: 1, Y: 2},
+	}
+
+	topright_r = []Cell[string]{
+		{X: 1, Y: 2},
+		{X: 1, Y: 1},
+		{X: 2, Y: 1},
+		{X: 0, Y: 1},
+		{X: 0, Y: 2},
+		{X: 0, Y: 0},
+		{X: 2, Y: 0},
+		{X: 1, Y: 0},
+	}
 )
 
 func init() {
@@ -61,36 +145,96 @@ func TestFill(t *testing.T) {
 	fmt.Printf("%#v\n", m2)
 }
 
-func TestAdjactent(t *testing.T) {
-	cells := m2.Adjacent(1, 1, false)
-	fmt.Printf("(1,1) %v %#v\n", len(cells), cells)
+func compare(l1, l2 []Cell[string]) bool {
+	if len(l1) != len(l2) {
+		fmt.Println("XXX compare length", l1, l2)
+		return false
+	}
 
-	cells = m2.Adjacent(0, 1, false)
-	fmt.Printf("(0,1) %v %#v\n", len(cells), cells)
+	sort.Slice(l1, func(i, j int) bool {
+		v1 := l1[i].X*3 + l1[i].Y
+		v2 := l1[j].X*3 + l1[j].Y
 
-	cells = m2.Adjacent(2, 1, false)
-	fmt.Printf("(2,1) %v %#v\n", len(cells), cells)
+		return v1 < v2
+	})
 
-	cells = m2.Adjacent(0, 0, false)
-	fmt.Printf("(1,0) %v %#v\n", len(cells), cells)
+	sort.Slice(l2, func(i, j int) bool {
+		v1 := l2[i].X*3 + l2[i].Y
+		v2 := l2[j].X*3 + l2[j].Y
 
-	cells = m2.Adjacent(2, 2, false)
-	fmt.Printf("(2,2) %v %#v\n", len(cells), cells)
+		return v1 < v2
+	})
+
+	for i := range l1 {
+		if l1[i].X != l2[i].X || l1[i].Y != l2[i].Y {
+			fmt.Println("XXX compare", i, l1, l2)
+			return false
+		}
+	}
+
+	return true
 }
 
-func TestAdjactentRoll(t *testing.T) {
-	cells := m2.Adjacent(1, 1, true)
-	fmt.Printf("(1,1) %v %#v\n", len(cells), cells)
+func TestAdjacent(t *testing.T) {
+	cells := m2.Adjacent(1, 1, false)
+	if !compare(cells, center) {
+		t.Logf("(1,1) %v %#v\n", len(cells), cells)
+		t.Fail()
+	}
 
-	cells = m2.Adjacent(0, 1, true)
-	fmt.Printf("(0,1) %v %#v\n", len(cells), cells)
+	cells = m2.Adjacent(0, 1, false)
+	if !compare(cells, left) {
+		t.Logf("(0,1) %v %#v\n", len(cells), cells)
+		t.Fail()
+	}
 
-	cells = m2.Adjacent(2, 1, true)
-	fmt.Printf("(2,1) %v %#v\n", len(cells), cells)
+	cells = m2.Adjacent(2, 1, false)
+	if !compare(cells, right) {
+		t.Logf("(2,1) %v %#v\n", len(cells), cells)
+		t.Fail()
+	}
 
 	cells = m2.Adjacent(0, 0, false)
-	fmt.Printf("(1,0) %v %#v\n", len(cells), cells)
+	if !compare(cells, bottomleft) {
+		t.Logf("(0,0) %v %#v\n", len(cells), cells)
+		t.Fail()
+	}
 
 	cells = m2.Adjacent(2, 2, false)
-	fmt.Printf("(2,2) %v %#v\n", len(cells), cells)
+	if !compare(cells, topright) {
+		t.Logf("(2,2) %v %#v\n", len(cells), cells)
+		t.Fail()
+	}
+}
+
+func TestAdjacentRoll(t *testing.T) {
+	cells := m2.Adjacent(1, 1, true)
+	if !compare(cells, center) {
+		t.Logf("(1,1) %v %#v\n", len(cells), cells)
+		t.Fail()
+	}
+
+	cells = m2.Adjacent(0, 1, true)
+	if !compare(cells, left_r) {
+		t.Logf("(0,1) %v %#v\n", len(cells), cells)
+		t.Fail()
+	}
+
+	cells = m2.Adjacent(2, 1, true)
+	if !compare(cells, right_r) {
+		t.Logf("(2,1) %v %#v\n", len(cells), cells)
+		t.Fail()
+	}
+
+	cells = m2.Adjacent(0, 0, true)
+	if !compare(cells, bottomleft_r) {
+		t.Logf("(0,0) %v %#v\n", len(cells), cells)
+		t.Fail()
+	}
+
+	cells = m2.Adjacent(2, 2, true)
+	if !compare(cells, topright_r) {
+		t.Logf("(2,2) %v %#v\n", len(cells), cells)
+		t.Fail()
+	}
 }
